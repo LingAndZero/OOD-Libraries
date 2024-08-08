d92019f510c9e270f91ca71adf4da1bc49606809
Ling <zhiweiling1999@outlook.com>
2024-08-08 22:13:39 +0800

    add file

---
+++ utils/utils.py
@@ -0,0 +1,15 @@
+import random
+import torch
+import numpy as np
+
+def fix_random_seed(seed):
+    """
+        fix all random seeds
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
