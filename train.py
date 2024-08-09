bc9298b9488135defb19ba8ff07822657750d011
Ling <zhiweiling1999@outlook.com>
2024-08-09 21:56:11 +0800

    add two model

--- train.py
+++ train.py
@@ -3,12 +3,43 @@ import torch
 import torch.nn as nn
 import argparse
 from tqdm import tqdm
+from torch.optim.lr_scheduler import MultiStepLR
 
 from utils.utils import fix_random_seed
 from utils.dataset import get_dataset
 from utils.models import get_model
 
 
+'''
+    ResNet-18 setting
+    epoch: 100
+    batch size: 128
+    learning rate: 0.1
+    weight decay: 5e-4
+    momentum: 0.9
+    lr decay: 0.1
+    lr decay epoch: 50, 75, 90
+'''
+'''
+    DenseNet-101 setting
+    epoch: 100
+    batch size: 64
+    learning rate: 0.1
+    weight decay: 1e-4
+    momentum: 0.9
+    lr decay: 0.1
+    lr decay epoch: 50, 75, 90
+'''
+'''
+    WideResNet-28 setting
+    epoch: 200
+    batch size: 128
+    learning rate: 0.1
+    weight decay: 5e-4
+    momentum: 0.9
+    lr decay: 0.1
+    lr decay epoch: 100, 150
+'''
 def get_train_options():
     parser = argparse.ArgumentParser()
 
@@ -16,10 +47,11 @@ def get_train_options():
     parser.add_argument("--model", type=str, default="DenseNet")
     parser.add_argument("--gpu", type=int, default=0)
 
-    parser.add_argument("--random_seed", type=int, default=2)
-    parser.add_argument("--epoch", type=int, default=200)
-    parser.add_argument("--bs", type=int, default=128)
+    parser.add_argument("--random_seed", type=int, default=0)
+    parser.add_argument("--epoch", type=int, default=100)
+    parser.add_argument("--bs", type=int, default=64)
     parser.add_argument("--lr", type=float, default=0.1)
+    parser.add_argument("--weight_decay", type=float, default=1e-4)
     parser.add_argument("--momentum", type=float, default=0.9)
     parser.add_argument('--num_classes', type=int, default=10)
 
@@ -59,13 +91,14 @@ if __name__ == '__main__':
 
     model = get_model(args).to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=5e-4)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+    scheduler = MultiStepLR(optimizer, milestones=[50, 75, 90], gamma=0.1)
 
-    for epoch in range(args.epoch):
+    for epoch in tqdm(range(args.epoch)):
         model.train()
         print("epoch: {}".format(epoch))
-        for batch_idx, (data, labels) in tqdm(enumerate(train_loader)):
+        for batch_idx, (data, labels) in enumerate(train_loader):
             data, labels = data.to(device), labels.to(device)
             optimizer.zero_grad()
             output = model(data)
@@ -76,6 +109,6 @@ if __name__ == '__main__':
         test(model, test_loader, device)
         scheduler.step()
 
-    filename = 'checkpoints/{}-{}-{}.pt'.format(args.dataset, args.model, args.random_seed)
+    filename = 'checkpoints/{}-{}-{}-{}.pt'.format(args.dataset, args.model, args.random_seed, args.epoch)
     os.makedirs(os.path.dirname(filename), exist_ok=True)
     torch.save(model.state_dict(), filename)
--- utils/models.py
+++ utils/models.py
@@ -1,16 +1,20 @@
 import torch
 from models.ResNet import *
 from models.DenseNet import *
+from models.WideResNet import *
 
 
 def get_model(args):
     model = None
 
-    if args.model == "resnet":
+    if args.model == "ResNet":
         model = resnet18(num_classes=args.num_classes)
 
     elif args.model == "DenseNet":
         model = DenseNet(growthRate=12, depth=100, reduction=0.5, bottleneck=True, nClasses=args.num_classes)
+
+    elif args.model == "WideResNet":
+        model = WideResNet(depth=28, num_classes=args.num_classes, widen_factor=1, droprate=0.0)
 
     return model
 
