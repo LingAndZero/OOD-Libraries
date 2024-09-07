85fc4cf865c3ad6ccce54b62a2432c5c81476185
Ling <zhiweiling1999@outlook.com>
2024-09-07 14:14:53 +0800

    rewrite ash method

--- models/ResNet_CIFAR.py
+++ models/ResNet_CIFAR.py
@@ -104,6 +104,15 @@ class ResNet(nn.Module):
         out = self.linear(out)
         return out
     
+    def feature(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        return out
+    
     def compute_threshold(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
--- ood_methods/ASH.py
+++ ood_methods/ASH.py
@@ -1,22 +1,6 @@
 import torch
 import numpy as np
-import torch.nn.functional as F
 from tqdm import tqdm
-
-
-def ash_eval(model, data_loader, device):
-    model.eval()
-    result = []
-
-    for (images, _) in tqdm(data_loader):
-        images = images.to(device)
-        output = model.ash_forward(images)
-
-        output = 1 * torch.logsumexp(output / 1, dim=1).data.cpu().numpy()
-
-        result.append(output)
-
-    return np.concatenate(result)
 
 
 class ASH:
@@ -28,7 +12,8 @@ class ASH:
         # Speical Parameters
         # temperature
         self.T = 1
-        self.p = 0.65
+        # Pruning Percentage
+        self.p = 65
 
     def eval(self, data_loader):
         self.model.eval()
@@ -36,10 +21,71 @@ class ASH:
 
         for (images, _) in tqdm(data_loader):
             images = images.to(self.device)
-            output = self.model.ash_forward(images)
+            feature = self.model.feature(images)
+
+            # Three pruning method: ash_p, ash_b, ash_s
+            output = ash_p(feature, self.p)
+            # output = ash_b(feature, self.p)
+            # output = ash_s(feature, self.p)
+
+            output = output.view(output.size(0), -1)
+            output = self.model.linear(output)
 
             output = self.T * torch.logsumexp(output / self.T, dim=1).data.cpu().numpy()
 
             result.append(output)
 
         return np.concatenate(result)
+
+
+
+def ash_b(x, percentile):
+    assert x.dim() == 4
+    assert 0 <= percentile <= 100
+    b, c, h, w = x.shape
+
+    s1 = x.sum(dim=[1, 2, 3])
+
+    n = x.shape[1:].numel()
+    k = n - int(np.round(n * percentile / 100.0))
+    t = x.view((b, c * h * w))
+    v, i = torch.topk(t, k, dim=1)
+    fill = s1 / k
+    fill = fill.unsqueeze(dim=1).expand(v.shape)
+    t.zero_().scatter_(dim=1, index=i, src=fill)
+    return x
+
+
+def ash_p(x, percentile):
+    assert x.dim() == 4
+    assert 0 <= percentile <= 100
+
+    b, c, h, w = x.shape
+
+    n = x.shape[1:].numel()
+    k = n - int(np.round(n * percentile / 100.0))
+    t = x.view((b, c * h * w))
+    v, i = torch.topk(t, k, dim=1)
+    t.zero_().scatter_(dim=1, index=i, src=v)
+
+    return x
+
+
+def ash_s(x, percentile):
+    assert x.dim() == 4
+    assert 0 <= percentile <= 100
+    b, c, h, w = x.shape
+
+    s1 = x.sum(dim=[1, 2, 3])
+    n = x.shape[1:].numel()
+    k = n - int(np.round(n * percentile / 100.0))
+    t = x.view((b, c * h * w))
+    v, i = torch.topk(t, k, dim=1)
+    t.zero_().scatter_(dim=1, index=i, src=v)
+
+    s2 = x.sum(dim=[1, 2, 3])
+
+    scale = s1 / s2
+    x = x * torch.exp(scale[:, None, None, None])
+
+    return x
